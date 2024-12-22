74dea1c827fb8aeb6bf44696c692f0e8f4450c68
Kiri <1936684518@qq.com>
2024-12-22 12:49:28 +0800

    [style]删除多余空格,添加注释

--- handler.py
+++ handler.py
@@ -4,8 +4,8 @@ from nonebot.plugin import on_message
 from nonebot.rule import regex
 from nonebot.adapters import Event, Message, Bot
 from nonebot_plugin_session import extract_session, SessionIdType
-
 from .config import Config
+##读取配置
 plugin_config = get_plugin_config(Config)
 plus = on_message(rule=regex(""), priority=plugin_config.plus_one_priority, block=False)
 msg_dict = {}
@@ -14,6 +14,7 @@ wait_enabled = plugin_config.plus_one_is_wait
 constThreshold = plugin_config.plus_one_threshold
 Plus_Threshold = constThreshold-1
 def get_url(s1:str)->str:
+    """获取图片url"""
     url = s1.split("fileid=")[1][:36]
     return url
 def is_equal(msg1: Message, msg2: Message)->bool:
@@ -24,31 +25,24 @@ def is_equal(msg1: Message, msg2: Message)->bool:
     if msg1 == msg2:
         return True
     return False
-
 def is_start(textlist:list)->bool:
     """判断能否复读"""
     return textlist.__len__()>=Plus_Threshold
-
 def doubled():
     global Plus_Threshold
     Plus_Threshold = max(Plus_Threshold,constThreshold)*2
-
 def go_back():
     global Plus_Threshold
     Plus_Threshold = constThreshold-1
-
 def check_word(message:Message)->bool:
     is_text = all(seg.is_text() for seg in message)
     if not is_text:
         return False
     elif message.__str__() in ignore_words:
         return True
-
-
 @plus.handle()
 async def plush_handler(bot: Bot, event: Event):
     global msg_dict
-
     session = extract_session(bot, event)
     group_id = session.get_id(SessionIdType.GROUP).split("_")[-1]
     if group_id not in plugin_config.plus_one_white_list:
@@ -58,7 +52,6 @@ async def plush_handler(bot: Bot, event: Event):
     if not text_list:
         text_list = []
         msg_dict[group_id] = text_list
-
     # 获取当前信息
     msg = event.get_message()
     if check_word(msg):
